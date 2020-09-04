afac96a488d8f0bf3189be3d373229514730a2d3
Rajiv Khanduja <49587262+rajiv786@users.noreply.github.com>
2020-09-04 12:41:29 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,12 +1,9 @@
-# Python program to draw
-# Rainbow Benzene
-# using Turtle Programming
 import turtle
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-t = turtle.Pen()
-turtle.bgcolor('black')
-for x in range(360):
-    t.pencolor(colors[x%6])
-    t.width(x/100 + 1)
-    t.forward(x)
+
+t = turtle.Turtle()
+list1 = ["purple", "red", "blue"]
+for i in range(200):
+    t.color(list1[i % 2])
+    t.pensize(i / 10 + 1)
+    t.forward(i)
     t.left(59)
